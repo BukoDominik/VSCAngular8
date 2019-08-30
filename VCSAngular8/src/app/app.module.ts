c5640711b1b3d35e0bdd7a9c340e576189caf457
BukoDominik <buko.dominik@gmail.com>
2019-08-30 02:08:16 +0200

    added modules

---
+++ VCSAngular8/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { MaterialModule} from './material.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
